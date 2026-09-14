a1b8f5f963a8a9f4b3a52a6d4a13e5ccabd6323e
gmlwls02190 <ksh02190@gmail.com>
2026-09-14 17:42:47 +0900

    [level 0] Title: 커피 심부름, Time: 0.11 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.py
@@ -0,0 +1,9 @@
+def solution(order):
+    answer = 0
+    for i in range(len(order)):
+        if "americano" in order[i] or "anything" in order[i]:
+            # print("check")
+            answer += 4500
+        elif "cafelatte" in order[i]:
+            answer += 5000
+    return answer
