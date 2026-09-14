4d63354dd7ccd64e0beaf6ecc243c86bbcc4e9d0
gmlwls02190 <ksh02190@gmail.com>
2026-09-14 17:48:40 +0900

    [level 0] Title: 배열 만들기 6, Time: 64.69 ms, Memory: 19.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.py
@@ -0,0 +1,13 @@
+def solution(arr):
+    answer = []
+    for i in range(len(arr)):
+        if not answer:
+            answer.append(arr[i])
+        elif answer and answer[-1]==arr[i]:
+            answer.pop()
+        elif answer and answer[-1]!=arr[i]:
+            answer.append(arr[i])
+    if not answer:
+        answer.append(-1)
+            
+    return answer
