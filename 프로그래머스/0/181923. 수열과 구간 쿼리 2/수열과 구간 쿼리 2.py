908bb2e8b71e56e395d59aec2527c52c0859c1e6
gmlwls02190 <ksh02190@gmail.com>
2026-09-15 02:03:39 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 7.27 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
@@ -2,13 +2,13 @@ def solution(arr, queries):
     answer = []
     for i in range(len(queries)):
         s,e,k = queries[i]
-        min = 0
+        min = 0 # 최소값 저장
         for j in range(s, e+1):
             if arr[j]>k:
-                if min==0 or min>arr[j]:
+                if min==0 or min>arr[j]: # min이 arr[j]보다 크다면 min에 arr[j] 저장
                     min = arr[j]
-        if min != 0:
+        if min != 0: # min이 0이 아니면 min을 추가
             answer.append(min)
-        else:
+        else: # 안쪽for문에서 min이 변경이 안됐다면 -1 추가
             answer.append(-1)
     return answer
